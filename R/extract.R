4bf5fbd0dd528c74c7b579df94a0237a9dd9c2b5
Bastiaan Quast <bquast@gmail.com>
2015-12-18 15:45:24 +0100

    other files

--- R/extract.R
+++ R/extract.R
@@ -3,4 +3,6 @@
 # bquast@gmail.com
 
 # unzip the zip-data in the data directory
-unzip(zipfile = 'data/rhsbaus-2011-2012-v1.zip', exdir = 'data')
+unzip(zipfile = 'data/nids-w1-2008-v5.3-20150619-sas.zip', exdir = 'data')
+unzip(zipfile = 'data/nids-w2-2010-2011-v2.3-20150619-sas.zip', exdir = 'data')
+unzip(zipfile = 'data/nids-w3-2012-v1.3-20150619-sas.zip', exdir = 'data')
