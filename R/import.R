df3113d9068d5da471a97bd98799debd286e50ba
Bastiaan Quast <bquast@gmail.com>
2015-12-18 15:55:26 +0100

    add household questionnaire importing

--- R/import.R
+++ R/import.R
@@ -17,12 +17,15 @@ adult3 <- read_sas(b7dat = 'data/nids-w3-2012-v1.3-20150619-sas/AdultW3AnonV1_3.
 child1 <- read_sas(b7dat = 'data/nids-v1-2008-v5.3-20150619-sas/ChildW1AnonV5_3.sas7bdat')
 child2 <- read_sas(b7dat = 'data/nids-w2-2010-2012-2.3-20150619-sas/ChildW2AnonV2_3.sas7bdat')
 child3 <- read_sas(b7dat = 'data/nids-w3-2012-v1.3-20150619-sas/ChildW3AnonV1_3.sas7bdat')
-inder1 <- read_sas(b7dat = 'data/nids-v1-2008-v5.3-20150619-sas/indderivedW1AnonV5_3.sas7bdat')
-inder2 <- read_sas(b7dat = 'data/nids-w2-2010-2012-2.3-20150619-sas/indderivedW2AnonV2_3.sas7bdat')
-inder3 <- read_sas(b7dat = 'data/nids-w3-2012-v1.3-20150619-sas/indderivedW3AnonV1_3.sas7bdat')
 hhder1 <- read_sas(b7dat = 'data/nids-v1-2008-v5.3-20150619-sas/hhderivedW1AnonV5_3.sas7bdat')
 hhder2 <- read_sas(b7dat = 'data/nids-w2-2010-2012-2.3-20150619-sas/hhderivedW2AnonV2_3.sas7bdat')
 hhder3 <- read_sas(b7dat = 'data/nids-w3-2012-v1.3-20150619-sas/hhderivedW3AnonV1_3.sas7bdat')
+hhque1 <- read_sas(b7dat = 'data/nids-v1-2008-v5.3-20150619-sas/HHQuestionnaireW1AnonV5_3.sas7bdat')
+hhque2 <- read_sas(b7dat = 'data/nids-w2-2010-2012-2.3-20150619-sas/HHQuestionnaireW2AnonV2_3.sas7bdat')
+hhque3 <- read_sas(b7dat = 'data/nids-w3-2012-v1.3-20150619-sas/HHQuestionnaireW3AnonV1_3.sas7bdat')
+inder1 <- read_sas(b7dat = 'data/nids-v1-2008-v5.3-20150619-sas/indderivedW1AnonV5_3.sas7bdat')
+inder2 <- read_sas(b7dat = 'data/nids-w2-2010-2012-2.3-20150619-sas/indderivedW2AnonV2_3.sas7bdat')
+inder3 <- read_sas(b7dat = 'data/nids-w3-2012-v1.3-20150619-sas/indderivedW3AnonV1_3.sas7bdat')
 
 
 # save to file
