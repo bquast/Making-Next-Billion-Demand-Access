e45f10b46049414ade1f80c1f2e94f61da937d79
Bastiaan Quast <bquast@gmail.com>
2015-12-24 09:44:44 +0100

    update for new data

--- R/estimate.R
+++ R/estimate.R
@@ -1,18 +1,21 @@
 # estimate.R
+# ----------------------
+# estimate models
+#
 # Bastiaan Quast
 # bquast@gmail.com
 
+
 # load libraries
-library(haven)
+library(dplyr)
+
 
 # load data
-load(file = "data/rhs.RData")
+load(file = "data/adult.RData")
 
-# encode
-rhs$internet <- ifelse(rhs$HI_1 == 1, TRUE, FALSE)
-rhs$language <- as_factor(rhs$N_7)
 
 # basic model
-m1 <- formula(HI_1 ~ N_7 + A_5)
+m1 <- formula(a_owncel ~ a_edlitrden)
+m2 <- formula(a_owncom ~ a_edlitrden)
 
-lm(m1, data = rhs)
+lm(m1, data = adult)
