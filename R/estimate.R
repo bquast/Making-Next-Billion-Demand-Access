94d87086a1c9a40e8c921e082ac4331ea6f33539
Bastiaan Quast <bquast@gmail.com>
2016-01-25 19:33:54 +0100

    m4 still working!!!

--- R/estimate.R
+++ R/estimate.R
@@ -19,20 +19,14 @@ ls(adulthh)
 
 
 # remove some missing value codes
-adulthh$a_edlitrdhm <- ifelse(adulthh$a_edlitrdhm < 0, NA, adulthh$a_edlitrdhm)
-adulthh$a_edlitrden <- ifelse(adulthh$a_edlitrden < 0, NA, adulthh$a_edlitrden)
+adulthh$a_edlitrdhm  <- ifelse(adulthh$a_edlitrdhm < 0, NA, adulthh$a_edlitrdhm)
+adulthh$a_edlitwrthm <- ifelse(adulthh$a_edlitwrthm < 0, NA, adulthh$a_edlitwrthm)
+adulthh$a_edlitrden  <- ifelse(adulthh$a_edlitrden < 0, NA, adulthh$a_edlitrden)
+adulthh$a_edlitwrten  <- ifelse(adulthh$a_edlitwrten < 0, NA, adulthh$a_edlitwrten)
 
 
-# some versions without missing value codes
-adulthh2 <- adulthh[!is.na(adulthh$h_nfcelspn),]
-adulthh3 <- adulthh2[adulthh2$h_nfcelspn != 0,]
-
-
-# inspect data
-summary(adulthh$a_lng)
-summary(adulthh$h_nftelspn)
-summary(adulthh$h_nfcelspn)
-summary(adulthh$h_nfnetspn)
+# create event dummy
+adulthh$post_event <- ifelse(adulthh$wave == 3, TRUE, FALSE)
 
 
 # exploratory
@@ -48,21 +42,17 @@ sumtable <- adulthh %>%
             netspn = mean(h_nfnetspn, na.rm = TRUE))
 
 
-# create event dummy
-adulthh$post_event <- ifelse(adulthh$wave == 3, TRUE, FALSE)
-
-
 # estimate models
 
 ## define models
-m1  <- formula(a_owncel ~ post_event*factor(a_lng) + a_edlitrden + a_edlitwrten + a_edlitrdhm + a_edlitwrthm + a_woman)
-m2  <- formula(a_owncom ~ post_event*factor(a_lng) + a_edlitrden + a_edlitwrten + a_edlitrdhm + a_edlitwrthm + a_woman)
-m3  <- formula(h_nfcel  ~ post_event*factor(a_lng) + a_edlitrden + a_edlitwrten + a_edlitrdhm + a_edlitwrthm + a_woman)
+m1  <- formula(a_owncel ~ post_event*factor(a_lng) + a_edlitrden + a_edlitwrten + a_edlitrdhm + a_edlitwrthm + a_woman + hhincome)
+m2  <- formula(a_owncom ~ post_event*factor(a_lng) + a_edlitrden + a_edlitwrten + a_edlitrdhm + a_edlitwrthm + a_woman + hhincome)
+m3  <- formula(h_nfcel  ~ post_event*factor(a_lng) + a_edlitrden + a_edlitwrten + a_edlitrdhm + a_edlitwrthm + a_woman + hhincome)
 m4  <- formula(h_nfnet  ~ post_event*factor(a_lng) + a_edlitrden + a_edlitwrten + a_edlitrdhm + a_edlitwrthm + a_woman)
 
 ## variations of m4
-m4_1  <- formula(h_nfnet  ~ factor(a_edlitrdhm)*post_event*factor(a_lng) + a_edlitrden + a_edlitwrten + a_edlitrdhm + a_edlitwrthm + a_woman)
-m4_2  <- formula(h_nfnet  ~ factor(a_edlitrden)*post_event*factor(a_lng) + a_edlitrden + a_edlitwrten + a_edlitrdhm + a_edlitwrthm + a_woman)
+m4_1  <- formula(h_nfnet  ~ post_event*factor(a_lng) + a_edlitrden + a_edlitrdhm + a_edlitwrthm + a_woman)
+m4_2  <- formula(h_nfnet  ~ factor(a_edlitrden)*post_event*factor(a_lng) + a_edlitrden + a_edlitwrten + a_edlitrdhm + a_edlitwrthm + a_woman + hhincome)
 
 ## estimate models
 lm1 <- lm(m1, data = adulthh)
@@ -80,3 +70,6 @@ summary(lm4)
 summary(lm4_1)
 summary(lm4_2)
 
+
+m_test <- formula(h_nfnet  ~ post_event*factor(a_lng) + a_edlitrden + a_edlitrdhm + a_edlitwrthm + a_woman)
+summary(lm(m_test, adulthh))
