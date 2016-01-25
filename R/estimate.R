4b5dc5498efddd2f06f9a3a3ab8d97d8ae1726b0
Bastiaan Quast <bquast@gmail.com>
2016-01-25 15:42:56 +0100

    move removal of missing value codes to merge file

--- R/estimate.R
+++ R/estimate.R
@@ -18,13 +18,18 @@ load(file = "data/merged.RData")
 ls(adulthh)
 
 
-# 
+# inspect data
+summary(adulthh$a_lng)
+summary(adulthh$h_nftelspn)
 summary(adulthh$h_nfcelspn)
+summary(adulthh$h_nfnetspn)
 
 
 # remove missing value codes
 adulthh$a_lng <- ifelse(adulthh$a_lng < 0, NA, adulthh$a_lng)
+adulthh$h_nftelspn <- ifelse(adulthh$h_nftelspn < 0, NA, adulthh$h_nftelspn)
 adulthh$h_nfcelspn <- ifelse(adulthh$h_nfcelspn < 0, NA, adulthh$h_nfcelspn)
+adulthh$h_nfnetspn <- ifelse(adulthh$h_nfnetspn < 0, NA, adulthh$h_nfnetspn)
 adulthh2 <- adulthh[!is.na(adulthh$h_nfcelspn),]
 adulthh3 <- adulthh2[adulthh2$h_nfcelspn != 0,]
 
