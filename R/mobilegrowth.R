7b942fd2a1e38a5bc9005072a8c61a63f167922b
Bastiaan Quast <bquast@gmail.com>
2016-05-04 15:34:51 +0200

    use saved data

--- R/mobilegrowth.R
+++ R/mobilegrowth.R
@@ -10,21 +10,23 @@
 library(reshape2)
 library(ggplot2)
 
+# load data
+load(file = 'data/mobilegrowth.RData')
 
-# import data
-df <- read.csv(file = 'data/MobileGrowth.csv')
-
-
-# rename and reshape
-names(df)[1] <- 'region'
-df <- gather(df, region)
-names(df)[2] <- 'year'
-names(df)[3] <- 'adoption'
-df$year <- as.numeric(sub('X', '', df$year))
-
+# # import data
+# df <- read.csv(file = 'data/MobileGrowth.csv')
+# 
+# 
+# # rename and reshape
+# names(df)[1] <- 'region'
+# df <- gather(df, region)
+# names(df)[2] <- 'year'
+# names(df)[3] <- 'adoption'
+# df$year <- as.numeric(sub('X', '', df$year))
+# ITU2015 <- df
 
 # plot
-ggplot(df, aes(x=year, y=adoption, colour=region)) + geom_smooth(se=FALSE)
+ggplot(ITU2015, aes(x=year, y=adoption, colour=region)) + geom_smooth(se=FALSE)
 
 
 # save data
