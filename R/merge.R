10fa723342ec7db103bf1a9bc28252da901e4836
Bastiaan Quast <bquast@gmail.com>
2016-05-19 14:07:39 +0200

    redo merging including wave 4

--- R/merge.R
+++ R/merge.R
@@ -49,7 +49,6 @@ vars_adult <- c('hhid',         # household ID
                 'a_intlng5',    # s1_5 - Languages used during interview
                 'a_intlng6')    # s1_6 - Languages used during interview
                 
-                
 vars_child <- c('hhid',       # household ID
                 'pid',        # person ID
                 'c_intrv_y',  # interview year
@@ -57,20 +56,21 @@ vars_child <- c('hhid',       # household ID
                 'c_intrv_d',  # interview day
                 'c_gen',      # gender
                 'c_dob_m',    # date-of-birth month
-                'c_dob_y',    # date-of-birth year
-                'c_mthhh',    # mother in household
-                'c_fthhh',    # father in household
-                'c_mthtrt',   # mother secondary school
-                'c_fthtrt')   # father secondary school
+                'c_dob_y'     # date-of-birth year
+                # 'c_mthhh',    # mother in household
+                # 'c_fthhh',    # father in household
+                # 'c_mthtrt',   # mother secondary school
+                # 'c_fthtrt'    # father secondary school
+                )
 
 vars_hhder <- c('hhid',       # household ID
                 'hhincome',   # household income
                 'expf',       # expenditure food?
                 'expnf',      # expenditure non-food?
                 'expenditure',# expenditure
-                'hhgeo2011',  # geo location for 2011
+                # 'hhgeo2011',  # geo location for 2011
                 'rentexpend', # rent expenditure
-                'hhimprent',  # imputed rent expenditure
+                # 'hhimprent',  # imputed rent expenditure
                 'hhagric')    # agricultural household
 
 vars_inder <- c('hhid',       # household ID
@@ -113,115 +113,147 @@ vars_hhque <- c('hhid',           # household identification
                 'h_owncel')       # f1_8 - The household owns a cellphone
 
 
-
 ## remove wave indicator from variable names
 names(adult1) %<>% 
   as.character() %>%
-  gsub(x = ., '^w[1-3].', "" )
+  gsub(x = ., '^w[1-4].', "" )
 names(adult2) %<>% 
   as.character() %>%
-  gsub(x = ., '^w[1-3].', "" )
+  gsub(x = ., '^w[1-4].', "" )
 names(adult3) %<>% 
   as.character() %>%
-  gsub(x = ., '^w[1-3].', "" )
+  gsub(x = ., '^w[1-4].', "" )
+names(adult4) %<>% 
+  as.character() %>%
+  gsub(x = ., '^w[1-4].', "" )
 names(child1) %<>% 
   as.character() %>%
-  gsub(x = ., '^w[1-3].', "" )
+  gsub(x = ., '^w[1-4].', "" )
 names(child2) %<>% 
   as.character() %>%
-  gsub(x = ., '^w[1-3].', "" )
+  gsub(x = ., '^w[1-4].', "" )
 names(child3) %<>% 
   as.character() %>%
-  gsub(x = ., '^w[1-3].', "" )
+  gsub(x = ., '^w[1-4].', "" )
+names(child4) %<>% 
+  as.character() %>%
+  gsub(x = ., '^w[1-4].', "" )
 names(hhder1) %<>% 
   as.character() %>%
-  gsub(x = ., '^w[1-3].', "" )
+  gsub(x = ., '^w[1-4].', "" )
 names(hhder2) %<>% 
   as.character() %>%
-  gsub(x = ., '^w[1-3].', "" )
+  gsub(x = ., '^w[1-4].', "" )
 names(hhder3) %<>% 
   as.character() %>%
-  gsub(x = ., '^w[1-3].', "" )
+  gsub(x = ., '^w[1-4].', "" )
+names(hhder4) %<>% 
+  as.character() %>%
+  gsub(x = ., '^w[1-4].', "" )
 names(inder1) %<>% 
   as.character() %>%
-  gsub(x = ., '^w[1-3].', "" )
+  gsub(x = ., '^w[1-4].', "" )
 names(inder2) %<>% 
   as.character() %>%
-  gsub(x = ., '^w[1-3].', "" )
+  gsub(x = ., '^w[1-4].', "" )
 names(inder3) %<>% 
   as.character() %>%
-  gsub(x = ., '^w[1-3].', "" )
+  gsub(x = ., '^w[1-4].', "" )
+names(inder4) %<>% 
+  as.character() %>%
+  gsub(x = ., '^w[1-4].', "" )
 names(hhque1) %<>% 
   as.character() %>%
-  gsub(x = ., '^w[1-3].', "" )
+  gsub(x = ., '^w[1-4].', "" )
 names(hhque2) %<>% 
   as.character() %>%
-  gsub(x = ., '^w[1-3].', "" )
+  gsub(x = ., '^w[1-4].', "" )
 names(hhque3) %<>% 
   as.character() %>%
-  gsub(x = ., '^w[1-3].', "" )
+  gsub(x = ., '^w[1-4].', "" )
+names(hhque4) %<>% 
+  as.character() %>%
+  gsub(x = ., '^w[1-4].', "" )
 
 ## homogenise some variables across waves
+hhder4$hhgeo2011  <- hhder4$geo2011
 hhque2$h_owncom   <- hhque2$h_wncom
 hhque2$h_nftelspn <- hhque2$h_nftel
 hhque2$h_nftel    <- ifelse(hhque2$h_nftel > 0, TRUE, ifelse(hhque2$h_nftel < 0, NA, FALSE))
 hhque1$h_nftelspn <- ifelse(hhque1$h_nftel == 2, 0, hhque1$h_nftelspn)
 hhque3$h_nftelspn <- ifelse(hhque3$h_nftel == 2, 0, hhque3$h_nftelspn)
+hhque4$h_nftelspn <- ifelse(hhque4$h_nftel == 2, 0, hhque4$h_nftelspn)
 hhque1$h_nftel    <- ifelse(hhque1$h_nftel == 1, TRUE, ifelse(hhque1$h_nftel == 2, FALSE, NA))
 hhque3$h_nftel    <- ifelse(hhque3$h_nftel == 1, TRUE, ifelse(hhque3$h_nftel == 2, FALSE, NA))
+hhque4$h_nftel    <- ifelse(hhque4$h_nftel == 1, TRUE, ifelse(hhque4$h_nftel == 2, FALSE, NA))
 hhque2$h_nfcelspn <- hhque2$h_nfcel
 hhque2$h_nfcel    <- ifelse(hhque2$h_nfcel > 0, TRUE, ifelse(hhque2$h_nfcel < 0, NA, FALSE))
 hhque1$h_nfcelspn <- ifelse(hhque1$h_nfcel == 2, 0, hhque1$h_nfcelspn)
 hhque3$h_nfcelspn <- ifelse(hhque3$h_nfcel == 2, 0, hhque3$h_nfcelspn)
+hhque4$h_nfcelspn <- ifelse(hhque4$h_nfcel == 2, 0, hhque4$h_nfcelspn)
 hhque1$h_nfcel    <- ifelse(hhque1$h_nfcel == 1, TRUE, ifelse(hhque1$h_nfcel == 2, FALSE, NA))
 hhque3$h_nfcel    <- ifelse(hhque3$h_nfcel == 1, TRUE, ifelse(hhque3$h_nfcel == 2, FALSE, NA))
+hhque4$h_nfcel    <- ifelse(hhque4$h_nfcel == 1, TRUE, ifelse(hhque4$h_nfcel == 2, FALSE, NA))
 hhque2$h_nfnetspn <- hhque2$h_nfnet
 hhque2$h_nfnet    <- ifelse(hhque2$h_nfnet > 0, TRUE, ifelse(hhque2$h_nfnet < 0, NA, FALSE))
 hhque1$h_nfnetspn <- ifelse(hhque1$h_nfnet == 2, 0, hhque1$h_nfnetspn)
 hhque3$h_nfnetspn <- ifelse(hhque3$h_nfnet == 2, 0, hhque3$h_nfnetspn)
+hhque4$h_nfnetspn <- ifelse(hhque4$h_nfnet == 2, 0, hhque4$h_nfnetspn)
 hhque1$h_nfnet    <- ifelse(hhque1$h_nfnet == 1, TRUE, ifelse(hhque1$h_nfnet == 2, FALSE, NA))
 hhque3$h_nfnet    <- ifelse(hhque3$h_nfnet == 1, TRUE, ifelse(hhque3$h_nfnet == 2, FALSE, NA))
+hhque4$h_nfnet    <- ifelse(hhque4$h_nfnet == 1, TRUE, ifelse(hhque4$h_nfnet == 2, FALSE, NA))
+
 
 ## subset to relevant variables
 adult1 %<>% subset(select=vars_adult)
 adult2 %<>% subset(select=vars_adult)
 adult3 %<>% subset(select=vars_adult)
+adult4 %<>% subset(select=vars_adult)
 child1 %<>% subset(select=vars_child)
 child2 %<>% subset(select=vars_child)
 child3 %<>% subset(select=vars_child)
+child4 %<>% subset(select=vars_child)
 hhder1 %<>% subset(select=vars_hhder)
 hhder2 %<>% subset(select=vars_hhder)
 hhder3 %<>% subset(select=vars_hhder)
+hhder4 %<>% subset(select=vars_hhder)
 inder1 %<>% subset(select=vars_inder)
 inder2 %<>% subset(select=vars_inder)
 inder3 %<>% subset(select=vars_inder)
+inder4 %<>% subset(select=vars_inder)
 hhque1 %<>% subset(select=vars_hhque)
 hhque2 %<>% subset(select=vars_hhque)
 hhque3 %<>% subset(select=vars_hhque)
+hhque4 %<>% subset(select=vars_hhque)
 
 ## add wave indicator
 adult1 %<>% cbind(wave = 1)
 adult2 %<>% cbind(wave = 2)
 adult3 %<>% cbind(wave = 3)
+adult4 %<>% cbind(wave = 4)
 child1 %<>% cbind(wave = 1)
 child2 %<>% cbind(wave = 2)
 child3 %<>% cbind(wave = 3)
+child4 %<>% cbind(wave = 4)
 hhder1 %<>% cbind(wave = 1)
 hhder2 %<>% cbind(wave = 2)
 hhder3 %<>% cbind(wave = 3)
+hhder4 %<>% cbind(wave = 4)
 inder1 %<>% cbind(wave = 1)
 inder2 %<>% cbind(wave = 2)
 inder3 %<>% cbind(wave = 3)
+inder4 %<>% cbind(wave = 4)
 hhque1 %<>% cbind(wave = 1)
 hhque2 %<>% cbind(wave = 2)
 hhque3 %<>% cbind(wave = 3)
+hhque4 %<>% cbind(wave = 4)
 
 ## merge inter-temporal
-adult <- rbind(adult1, adult2, adult3)
-child <- rbind(child1, child2, child3)
-hhder <- rbind(hhder1, hhder2, hhder3)
-inder <- rbind(inder1, inder2, inder3)
-hhque <- rbind(hhque1, hhque2, hhque3)
+adult <- rbind(adult1, adult2, adult3, adult4)
+child <- rbind(child1, child2, child3, child4)
+hhder <- rbind(hhder1, hhder2, hhder3, hhder4)
+inder <- rbind(inder1, inder2, inder3, inder4)
+hhque <- rbind(hhque1, hhque2, hhque3, hhque4)
 
 
 # recode certain variables
@@ -244,9 +276,9 @@ save(file = 'data/hhque.RData', hhque)
 
 # merge across data.frame types
 adult   <- merge(adult,   inder, by = c('pid', 'hhid', 'wave'), all.x = TRUE)
-adult$hhid <- adult$hhid.x
-adult$hhid.x <- NULL
-adult$hhid.y <- NULL
+# adult$hhid <- adult$hhid.x
+# adult$hhid.x <- NULL
+# adult$hhid.y <- NULL
 adulthh <- merge(adult,   hhder, by = c('hhid', 'wave'), all.x = TRUE)
 adulthh <- merge(adulthh, hhque, by = c('hhid', 'wave'), all.x = TRUE)
 
@@ -289,7 +321,6 @@ adulthh$a_lng <- factor(adulthh$a_lng, labels = c('IsiNdebele',
                                                   'IsiTsonga',
                                                   'Afrikaans',
                                                   'English'))
-
 
 
 # save to file
