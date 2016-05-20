e61b632d1b066a6618b430523031f100f1852384
Bastiaan Quast <bquast@gmail.com>
2016-05-20 15:29:54 +0200

    extract new versions

--- R/extract.R
+++ R/extract.R
@@ -3,7 +3,7 @@
 # bquast@gmail.com
 
 # unzip the zip-data in the data directory
-unzip(zipfile = 'data/nids-w1-2008-v5.3-20150619-sas.zip',      exdir = 'data')
-unzip(zipfile = 'data/nids-w2-2010-2011-v2.3-20150619-sas.zip', exdir = 'data')
-unzip(zipfile = 'data/nids-w3-2012-v1.3-20150619-sas.zip',      exdir = 'data')
-unzip(zipfile = 'data/nids-w4-2014-2015-v1-stata12.zip',        exdir = 'data')
+unzip(zipfile = 'data/nids-w1-2008-v6-stata12.zip',      exdir = 'data')
+unzip(zipfile = 'data/nids-w2-2010-2011-v3-stata12.zip', exdir = 'data')
+unzip(zipfile = 'data/nids-w3-2012-v2-stata12.zip',      exdir = 'data')
+unzip(zipfile = 'data/nids-w4-2014-2015-v1-stata12.zip', exdir = 'data')
