b38ca2607380fdbf572da41e4b8509dd25a2deb8
Bastiaan Quast <bquast@gmail.com>
2016-10-21 09:14:52 +0200

    add extracting for August 2016 files, including wave 4 (1.1)

--- R/extract.R
+++ R/extract.R
@@ -7,3 +7,10 @@ unzip(zipfile = 'data/nids-w1-2008-v5.3-20150619-sas.zip',      exdir = 'data')
 unzip(zipfile = 'data/nids-w2-2010-2011-v2.3-20150619-sas.zip', exdir = 'data')
 unzip(zipfile = 'data/nids-w3-2012-v1.3-20150619-sas.zip',      exdir = 'data')
 unzip(zipfile = 'data/nids-w4-2014-2015-v1-stata12.zip',        exdir = 'data')
+
+
+# unzip the zip-data in the data directory (August 2016)
+unzip(zipfile = 'data/nids-w1-2008-v6.1-sas.zip',      exdir = 'data')
+unzip(zipfile = 'data/nids-w2-2010-2011-v3.1-sas.zip', exdir = 'data')
+unzip(zipfile = 'data/nids-w3-2012-v2.1-sas.zip',      exdir = 'data')
+unzip(zipfile = 'data/nids-w4-2014-2015-v1.1-sas.zip', exdir = 'data')
